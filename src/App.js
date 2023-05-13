63c32ac80697f4103021f8c3df674a03c727b7d2
Dilshad Karim <dilshad>
2023-05-13 09:10:15 +0500

    Hero Banner added to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
+import React from "react";
+import { Route,Routes} from "react-router-dom";
+import { Box } from "@mui/material";
 import './App.css';
+import Home from './Pages/Home';
+import ExerciseDetails from './Pages/ExerciseDetails';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>hello World</h1>
-    </div>
-  );
+const App =() =>{
+    return(
+        <Box width='400px' sx={{width:{xl:'1488'}}} m='auto'>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/exercise/:id" element={<ExerciseDetails />}/>
+            </Routes>
+            <Footer/>
+        </Box>
+    )
 }
 
-export default App;
+export default App
