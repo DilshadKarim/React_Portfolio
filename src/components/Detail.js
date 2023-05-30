9638b00960fac2e650f3ecebe20cb7d13a08e2b4
Dilshad Karim <dilshad>
2023-05-30 23:30:24 +0500

    my last commit for this project

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,14 +1,46 @@
 import React from 'react';
-import {Typography, Stack,Buttton} from '@mui/material';
+import {Typography, Stack, Button} from '@mui/material';
 import BodyPartImage from '../assets/icons/body-part.png';
 import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({exerciseDetail}) => {
   const{bodyPart,gifUrl, name, target, equipment} = exerciseDetail;
+  const extraDetail =[
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ]
   return (
     <Stack gap="60px" sx={{flexDirection:{lg:'row'}, p:'20px', alignItems:'center'}}>
       <img src={gifUrl} alt={name} loading='lazy' className='detail-image'/>
+      <Stack sx={{ gap:{lg:'35px', xs : '20px'}}}>
+        <Typography variant='h6'>
+          {name}
+        </Typography>
+        <Typography>
+          Exercise keep you strong. {name} {' '} is one of the best exercises to target your abs. It will help you improve your mood and gain energy.
+        </Typography>
+          {extraDetail.map((item)=>(
+            <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+              <Button sx={{background: '#ff2db', borderRadius: '50px', width:'100px', height:'100px'}}>
+                <img src={item.icon} alt={bodyPart} sttyle={{width:'50px', height:'50px'}}/>
+              </Button>
+              <Typography textTransform='capitalize' variant='h6'>
+                {item.name}
+              </Typography>
+            </Stack>
+          ))}
+      </Stack>
     </Stack>
   );
 };
